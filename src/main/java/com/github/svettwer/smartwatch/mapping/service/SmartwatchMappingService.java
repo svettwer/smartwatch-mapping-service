bb39fff39d687f45268946493ae4203bc694866b
Sven Hettwer <sven.hettwer@consol.de>
2019-03-13 13:47:32 +0100

    Added spring boot + kafka

---
+++ src/main/java/com/github/svettwer/smartwatch/mapping/service/SmartwatchMappingService.java
@@ -0,0 +1,13 @@
+package com.github.svettwer.smartwatch.mapping.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwatchMappingService {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SmartwatchMappingService.class, args);
+    }
+}
