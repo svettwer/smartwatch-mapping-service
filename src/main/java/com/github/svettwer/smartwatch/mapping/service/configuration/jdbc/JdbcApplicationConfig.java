4fa232960e771140742d789b05199c046eee5684
Sven Hettwer <sven.hettwer@consol.de>
2019-05-20 13:41:35 +0200

    Removed jpa transaction management

--- src/main/java/com/github/svettwer/smartwatch/mapping/service/configuration/jdbc/JdbcApplicationConfig.java
+++ src/main/java/com/github/svettwer/smartwatch/mapping/service/configuration/jdbc/JdbcApplicationConfig.java
@@ -6,11 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.github.svettwer.smartwatch.mapping.service.database")
-@EnableTransactionManagement
 public class JdbcApplicationConfig {
 
     private Environment env;
