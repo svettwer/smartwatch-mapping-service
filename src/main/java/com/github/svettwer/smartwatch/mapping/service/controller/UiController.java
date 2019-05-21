44cadd0e85b9541bca2d6c8bd1749929a236f310
Sven Hettwer <sven.hettwer@consol.de>
2019-05-21 10:44:32 +0200

    Added table view of mapping data

--- src/main/java/com/github/svettwer/smartwatch/mapping/service/controller/UiController.java
+++ src/main/java/com/github/svettwer/smartwatch/mapping/service/controller/UiController.java
@@ -1,5 +1,7 @@
 package com.github.svettwer.smartwatch.mapping.service.controller;
 
+import com.github.svettwer.smartwatch.mapping.service.service.PairingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +12,17 @@ public class UiController {
     @Value("${spring.application.name}")
     String appName;
 
+    private PairingService pairingService;
 
+    @Autowired
+    public UiController(final PairingService pairingService) {
+        this.pairingService = pairingService;
+    }
 
     @GetMapping("/")
     public String homePage(final Model model) {
         model.addAttribute("appName", appName);
+        model.addAttribute("pairings", pairingService.getPairings());
         return "home";
     }
 }
